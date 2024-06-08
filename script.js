a531b6f2d078a2e1c575dea1741c903922f22031
Zachariah Prajeeyachat <titan.zp4@gmail.com>
2024-06-08 21:18:11 +0900

    First commit
    Navigation bar (mobile first)

---
+++ script.js
@@ -0,0 +1,33 @@
+let menuOpen = false;
+let megaOpen = false;   
+
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu-mobile');
+const menuButton = document.getElementById('menu-button');
+const mega = document.getElementById('menu-desktop');
+
+const toggleMenu = () => {
+   menuOpen = !menuOpen;
+   if (menuOpen === true) {
+      menu.className = menu.className.replace(' hidden', '');
+   } else {
+      menu.className = menu.className + ' hidden';
+   }
+}
+
+const toggleMega = () => {
+   megaOpen = !megaOpen;
+   if (megaOpen === true) {
+      mega.className = mega.className.replace(' sm-hide', '');
+      mega.className = mega.className + ' sm-g-4 '
+
+   } else {
+      mega.className = mega.className + ' sm-hide';
+      mega.className = mega.className.replace(' sm-g-4', '')
+
+   }
+   console.log(megaOpen);
+}
+
+hamburger.addEventListener('click', toggleMenu);
+menuButton.addEventListener('click', toggleMega)

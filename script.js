8be7a055081b460b8444436cb07750099c5f728a
JuthathipLoedsinaudom <59654072+jjennyzs@users.noreply.github.com>
2024-06-19 14:25:22 +0900

    Carousel on html

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu-mobile');
 const menuButton = document.getElementById('menu-button');
 const mega = document.getElementById('menu-desktop');
+const addToCart = document.getElementById('add-to-cart');
+const backButton = document.getElementById('back');
+const nextButton = document.getElementById('next');
+const display = document.getElementById('display');
 
 const toggleMenu = () => {
    menuOpen = !menuOpen;
@@ -19,15 +23,43 @@ const toggleMega = () => {
    megaOpen = !megaOpen;
    if (megaOpen === true) {
       mega.className = mega.className.replace(' sm-hide', '');
-      mega.className = mega.className + ' sm-g-4 '
+      mega.className = mega.className + ' sm-g-4 ';
 
    } else {
       mega.className = mega.className + ' sm-hide';
-      mega.className = mega.className.replace(' sm-g-4', '')
+      mega.className = mega.className.replace(' sm-g-4', '');
 
    }
-   console.log(megaOpen);
 }
 
+const addItem = () => {
+   alert('Item added to cart');
+}
+
+// function addItem() {
+//    alert('Item added to cart');
+// }
+
+let picIndex = 0;
+
+const changePic = move => {
+   const newIndex = picIndex += move;
+   if (newIndex === 20) {
+      picIndex = 0;
+   } else if (newIndex === -1) {
+      picIndex = 19;
+   }
+   display.src = './public/image/slides/' + picIndex + '.jpg';
+}
+
+setInterval(() => changePic(1), 2000);
+
 hamburger.addEventListener('click', toggleMenu);
-menuButton.addEventListener('click', toggleMega)
+menuButton.addEventListener('click', toggleMega);
+
+if (addToCart) {
+   addToCart.addEventListener('click', addItem);
+}
+
+backButton.addEventListener('click', () => changePic(-1));
+nextButton.addEventListener('click', () => changePic(1))
